83a60c90a77e70a15e813fcfff4c8a0125e3f91f
Edvam S. (Nine_Nine) <edvamsantos1998@gmail.com>
2024-05-16 15:48:18 -0300

    geral

--- client/src/banco/database.js
+++ client/src/banco/database.js
@@ -1,12 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Conectar ao banco de dados SQLite3
-const db = new sqlite3.Database('./src/banco/bancogeral.db', (err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err.message);
-  } else {
-    console.log('Conexão bem-sucedida com o banco de dados SQLite3');
-  }
+const db = new sqlite3.Database('../client/src/banco/bancogeral.db', (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err.message);
+    } else {
+        console.log('Conexão bem-sucedida com o banco de dados SQLite3');
+    }
 });
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-const express = require ('express')
-const app = express()
+const express = require('express');
+const db = require('../client/src/banco/database'); // Importar o arquivo database.js
+const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Server is running");
-  }); 
+// Rotas da sua aplicação
+app.get('/', (req, res) => {
+    // Exemplo de consulta ao banco de dados
+    db.all('SELECT * FROM tabela', (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: rows });
+    });
+});
 
-app.listen(5000, console.log("Server started on PORT 5000"));
- 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
