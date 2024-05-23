553d391e1d78d8512aca12569f6ff63cb5c18c10
Edvam Santos <edvamsantos1998@gmail.com>
2024-05-22 21:44:24 -0300

    Sistema de Banco de Dados Exemplo

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/MitoCoder"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          With my thanks Edvam Santos
         </a>
       </header>
     </div>
