25cad45b1a495078e4e57312c40fcf692e911e8b
Edvam Santos <edvamsantos1998@gmail.com>
2024-05-22 21:49:56 -0300

    Sistema de Banco de Dados Exemplo

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 
 app.use("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server is running - By Edvam Santos");
   }); 
 
 app.listen(5000, console.log("Server started on PORT 5000"));
